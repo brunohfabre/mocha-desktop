e0b9205ee9ee81d9dc8d1ba7a55ad6074da67b9b
Bruno Fabre <bruno.hfabre@gmail.com>
2024-10-15 08:41:03 -0300

    many changes to environments

--- src/pages/collections/collection/environments.tsx
+++ src/pages/collections/collection/environments.tsx
@@ -1,39 +1,81 @@
+import { useState } from 'react'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
-const environments = [
-  {
-    id: crypto.randomUUID(),
-    name: 'Local',
-    variables: {
-      base_url: 'base url',
-      token: 'token 1',
-    },
-  },
-  {
-    id: crypto.randomUUID(),
-    name: 'Staging',
-    variables: {
-      base_url: 'base url',
-      token: 'token 2',
-    },
-  },
-  {
-    id: crypto.randomUUID(),
-    name: 'Production',
-    variables: {
-      base_url: 'base url',
-      token: 'token 3',
-    },
-  },
-]
-
 export function Environments() {
-  const variables = Object.keys(environments[0].variables)
+  const [environments, setEnvironments] = useState([
+    {
+      id: crypto.randomUUID(),
+      name: 'Local',
+    },
+    {
+      id: crypto.randomUUID(),
+      name: 'Staging',
+    },
+    {
+      id: crypto.randomUUID(),
+      name: 'Production',
+    },
+  ])
+  const [variables, setVariables] = useState([
+    {
+      id: crypto.randomUUID(),
+      name: 'BASE_URL',
+    },
+    {
+      id: crypto.randomUUID(),
+      name: 'TOKEN',
+    },
+  ])
+  const [values, setValues] = useState([
+    {
+      environmentId: 'env-1',
+      variableId: 'var-1',
+      value: 'value-1',
+    },
 
-  function handleAddEnvironment() { }
+    {
+      environmentId: 'env-2',
+      variableId: 'var-2',
+      value: 'value-2',
+    },
 
-  function handleAddVariable() { }
+    {
+      environmentId: 'env-3',
+      variableId: 'var-1',
+      value: 'value-3',
+    },
+  ])
+
+  function handleAddEnvironment() {
+    console.log('handle add environment')
+  }
+
+  function handleAddVariable() {
+    setVariables((prevState) => [
+      ...prevState,
+      {
+        id: crypto.randomUUID(),
+        name: '',
+      },
+    ])
+  }
+
+  function handleChangeVariable(data: { variableId: string; value: string }) {
+    console.log('handle change variable', data)
+  }
+
+  function handleChangeEnvironment(data: {
+    environmentId: string
+    value: string
+  }) {
+    console.log('handle change environment', data)
+  }
+
+  function handleChangeValue(data: any) {
+    console.log('handle change value', data)
+  }
 
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
@@ -48,25 +90,52 @@ export function Environments() {
 
             {environments.map((environment) => (
               <td key={environment.id}>
-                <Input value={environment.name} />
+                <Input
+                  value={environment.name}
+                  onChange={(event) =>
+                    handleChangeEnvironment({
+                      environmentId: environment.id,
+                      value: event.target.value,
+                    })
+                  }
+                />
               </td>
             ))}
 
             <td>
-              <Button size="icon">+</Button>
+              <Button size="icon" onClick={handleAddEnvironment}>
+                +
+              </Button>
             </td>
           </tr>
         </thead>
+
         <tbody>
           {variables.map((variable) => (
-            <tr key={variable}>
+            <tr key={variable.id}>
               <td>
-                <Input value={variable} />
+                <Input
+                  value={variable.name}
+                  onChange={(event) =>
+                    handleChangeVariable({
+                      variableId: variable.id,
+                      value: event.target.value,
+                    })
+                  }
+                />
               </td>
 
               {environments.map((environment) => (
                 <td key={environment.id}>
-                  <Input />
+                  <Input
+                    onChange={(event) =>
+                      handleChangeValue({
+                        environmentId: environment.id,
+                        variableId: variable.id,
+                        value: event.target.value,
+                      })
+                    }
+                  />
                 </td>
               ))}
             </tr>
@@ -74,7 +143,9 @@ export function Environments() {
 
           <tr>
             <td>
-              <Button size="icon">+</Button>
+              <Button size="icon" onClick={handleAddVariable}>
+                +
+              </Button>
             </td>
           </tr>
         </tbody>
