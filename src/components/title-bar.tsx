411bf856e2be5415484d58e33e52910840811e83
Bruno Fabre <bruno.hfabre@gmail.com>
2025-04-17 17:03:33 -0300

    add svg

--- src/components/title-bar.tsx
+++ src/components/title-bar.tsx
@@ -13,31 +13,33 @@ export function TitleBar() {
     <div className="h-[52px] flex">
       {isMacos && <div className="h-[52px] w-[92px]" data-tauri-drag-region />}
 
-      {!!token && (
-        <div className="flex-1 flex" data-tauri-drag-region>
-          <div className="p-2 pl-0">
-            <Button size="icon" variant="outline">
-              <House size={16} />
-            </Button>
-          </div>
+      <div className="flex-1 flex" data-tauri-drag-region>
+        {!!token && (
+          <>
+            <div className="p-2 pl-0">
+              <Button size="icon" variant="outline">
+                <House size={16} />
+              </Button>
+            </div>
 
-          <div className="py-4">
-            <Separator orientation="vertical" />
-          </div>
+            <div className="py-4">
+              <Separator orientation="vertical" />
+            </div>
 
-          <div className="flex-1" data-tauri-drag-region />
+            <div className="flex-1" data-tauri-drag-region />
 
-          <div className="py-4">
-            <Separator orientation="vertical" />
-          </div>
+            <div className="py-4">
+              <Separator orientation="vertical" />
+            </div>
 
-          <div className="p-2">
-            <Button size="icon" variant="outline">
-              <Search size={16} />
-            </Button>
-          </div>
-        </div>
-      )}
+            <div className="p-2">
+              <Button size="icon" variant="outline">
+                <Search size={16} />
+              </Button>
+            </div>
+          </>
+        )}
+      </div>
 
       {!isMacos && <div className="bg-green-500 w-32" />}
     </div>
