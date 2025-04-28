5efcfdbca543d03e25776769651c4dd6cd2b9f44
Bruno Fabre <bruno.hfabre@gmail.com>
2025-04-28 12:26:08 -0300

    fix

--- src/components/title-bar.tsx
+++ src/components/title-bar.tsx
@@ -5,38 +5,26 @@ import CloseIcon from '@/assets/icons/close.svg'
 import MaximizeIcon from '@/assets/icons/maximize.svg'
 import MinimizeIcon from '@/assets/icons/minimize.svg'
 import RestoreIcon from '@/assets/icons/restore.svg'
-import { useState } from 'react'
+import { type ReactNode, useState } from 'react'
 
 const appWindow = getCurrentWindow()
 
 interface TitleBarProps {
-  showButtons?: boolean
+  children: ReactNode
 }
 
-export function TitleBar({ showButtons = true }: TitleBarProps) {
+export function TitleBar({ children }: TitleBarProps) {
   const [isMaximized, setIsMaximized] = useState(false)
 
   const isMacos = platform() === 'macos'
 
   return (
     <div className="h-[52px] flex">
-      {isMacos && <div className="h-[52px] w-[92px]" data-tauri-drag-region />}
+      {true && <div className="h-[52px] w-[92px]" data-tauri-drag-region />}
 
-      <div className="flex-1 flex">
-        {!!showButtons && (
-          <>
-            <div className="flex-1" data-tauri-drag-region />
+      <div className="flex-1">{children}</div>
 
-            {/* <div className="p-2">
-              <Button size="icon" variant="outline">
-                <Search size={16} />
-              </Button>
-            </div> */}
-          </>
-        )}
-      </div>
-
-      {!isMacos && (
+      {true && (
         <div className="flex">
           <button
             type="button"
