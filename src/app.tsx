21f9502985df540e1188de1a8cf13771aa062f67
Bruno Fabre <bruno.hfabre@gmail.com>
2025-08-07 17:22:36 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <Button>Button</Button>
+    </div>
+  )
+}
