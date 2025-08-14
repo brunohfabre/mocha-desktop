124d402645c83f895585c9b1a7af739795b48c83
Bruno Fabre <bruno.hfabre@gmail.com>
2025-08-14 08:54:22 -0300

    add single instance

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 use tauri_plugin_deep_link::DeepLinkExt;
+use tauri::{AppHandle, Manager};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -7,21 +8,22 @@ pub fn run() {
 
     #[cfg(desktop)]
     {
-        builder = builder.plugin(tauri_plugin_single_instance::init(|_app, argv, _cwd| {
-            println!("single instance triggered: {argv:?}");
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, argv, _cwd| {
+            let _ = app.get_webview_window("main")
+                        .expect("no main window")
+                        .set_focus();
+
+          println!("a new app instance was opened with {argv:?} and the deep link event was already triggered");
+          // when defining deep link schemes at runtime, you must also check `argv` here
         }));
     }
-    
+
     builder
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
             #[cfg(any(target_os = "linux", all(debug_assertions, windows)))]
             app.deep_link().register_all()?;
-
-            app.deep_link().on_open_url(|event| {
-                dbg!(event.urls());
-            });
 
             Ok(())
         })
