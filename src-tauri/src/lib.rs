e2cad0d4a29cf4fa1899ffc6f2d3ab76e60db44f
Bruno Fabre <bruno.hfabre@gmail.com>
2025-08-22 16:27:17 -0300

    fix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -57,25 +57,6 @@ pub fn run() {
                 }
             }
 
-            // // set background color only when building for macOS
-            // #[cfg(target_os = "macos")]
-            // {
-            //     use cocoa::appkit::{NSColor, NSWindow};
-            //     use cocoa::base::{id, nil};
-
-            //     let ns_window = window.ns_window().unwrap() as id;
-            //     unsafe {
-            //         let bg_color = NSColor::colorWithRed_green_blue_alpha_(
-            //             nil,
-            //             50.0 / 255.0,
-            //             158.0 / 255.0,
-            //             163.5 / 255.0,
-            //             1.0,
-            //         );
-            //         ns_window.setBackgroundColor_(bg_color);
-            //     }
-            // }
-
             Ok(())
         })
         .run(tauri::generate_context!())
