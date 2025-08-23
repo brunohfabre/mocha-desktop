c383bd4d331cc98bc8cd19c345ecb73b7ffa4338
Bruno Fabre <bruno.hfabre@gmail.com>
2025-08-23 18:02:20 -0300

    add more features

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,11 @@
 import { Link, useNavigate } from '@tanstack/react-router'
+import { getCurrentWindow } from '@tauri-apps/api/window'
 import { platform } from '@tauri-apps/plugin-os'
+import { useState } from 'react'
+import MaximizeIcon from '@/assets/icons/maximize.svg'
+import MinusIcon from '@/assets/icons/minus.svg'
+import ResizeIcon from '@/assets/icons/resize.svg'
+import XIcon from '@/assets/icons/x.svg'
 import Logo from '@/assets/logo.png'
 import { useAuthStore } from '@/stores/auth'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
@@ -22,6 +28,8 @@ export function Header() {
   const user = useAuthStore((state) => state.user)
   const clearCredentials = useAuthStore((state) => state.clearCredentials)
 
+  const [isMaximized, setIsMaximized] = useState(false)
+
   function handleSignOut() {
     clearCredentials()
 
@@ -31,65 +39,119 @@ export function Header() {
     })
   }
 
+  async function handleMinimize() {
+    await getCurrentWindow().minimize()
+  }
+
+  async function handleToggleMaximize() {
+    await getCurrentWindow().toggleMaximize()
+
+    setIsMaximized((prevState) => !prevState)
+  }
+
+  async function handleClose() {
+    await getCurrentWindow().close()
+  }
+
   return (
     <>
-      <div className="h-10 w-full flex" data-tauri-drag-region>
-        {token && (
-          <>
-            {currentPlatform === 'macos' && (
-              <>
-                <div className="w-20" data-tauri-drag-region />
+      <div className="h-10 w-full flex">
+        <div className="flex-1" data-tauri-drag-region>
+          {token && (
+            <>
+              {currentPlatform === 'macos' && (
+                <>
+                  <div className="w-20" data-tauri-drag-region />
 
-                <Separator orientation="vertical" />
-              </>
-            )}
+                  <Separator orientation="vertical" />
+                </>
+              )}
 
-            <Link
-              to="/"
-              className="px-3 flex items-center gap-1.5"
-              data-tauri-drag-region
-            >
-              <img src={Logo} alt="" className="max-h-6" />
-
-              <span className="text-base font-bold">Mocha</span>
-            </Link>
-
-            <Separator orientation="vertical" />
-
-            <div className="flex-1" data-tauri-drag-region />
-
-            <Separator orientation="vertical" />
-
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <div className="size-10 flex items-center justify-center">
-                  <Avatar>
-                    <AvatarImage src={user?.avatarUrl} />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </div>
-              </DropdownMenuTrigger>
-
-              <DropdownMenuContent
-                align="end"
-                className="min-w-48"
-                alignOffset={4}
+              <Link
+                to="/"
+                className="px-3 flex items-center gap-1.5"
+                data-tauri-drag-region
               >
-                <DropdownMenuLabel className="p-0 font-normal">
-                  <div className="flex items-center px-2 py-1.5 text-left text-sm">
-                    <div className="grid flex-1 text-left text-sm leading-tight">
-                      <span className="truncate font-medium">{user?.name}</span>
-                      <span className="truncate text-xs">{user?.email}</span>
-                    </div>
+                <img src={Logo} alt="" className="max-h-6" />
+
+                <span className="text-base font-bold">Mocha</span>
+              </Link>
+
+              <Separator orientation="vertical" />
+
+              <div className="flex-1" data-tauri-drag-region />
+
+              <Separator orientation="vertical" />
+
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <div className="size-10 flex items-center justify-center">
+                    <Avatar>
+                      <AvatarImage src={user?.avatarUrl} />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
                   </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={handleSignOut}>
-                  Log out
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent
+                  align="end"
+                  className="min-w-48"
+                  alignOffset={4}
+                >
+                  <DropdownMenuLabel className="p-0 font-normal">
+                    <div className="flex items-center px-2 py-1.5 text-left text-sm">
+                      <div className="grid flex-1 text-left text-sm leading-tight">
+                        <span className="truncate font-medium">
+                          {user?.name}
+                        </span>
+                        <span className="truncate text-xs">{user?.email}</span>
+                      </div>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={handleSignOut}>
+                    Log out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          )}
+        </div>
+
+        {currentPlatform !== 'macos' && (
+          <div>
+            <button
+              type="button"
+              className="h-10 px-4 hover:bg-muted transition-colors cursor-pointer"
+              onClick={handleMinimize}
+            >
+              <img src={MinusIcon} alt="" />
+            </button>
+
+            <button
+              type="button"
+              className="h-10 px-4 hover:bg-muted transition-colors cursor-pointer"
+              onClick={handleToggleMaximize}
+            >
+              {isMaximized ? (
+                <img src={ResizeIcon} alt="" />
+              ) : (
+                <img src={MaximizeIcon} alt="" />
+              )}
+            </button>
+
+            <button
+              type="button"
+              className="h-10 px-4 hover:bg-destructive group transition-colors cursor-pointer"
+              onClick={handleClose}
+            >
+              <img
+                src={XIcon}
+                alt=""
+                className="group-hover:invert transition-colors"
+              />
+            </button>
+          </div>
         )}
       </div>
 
--- src/pages/_auth/sign-in.tsx
+++ src/pages/_auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { openUrl } from '@tauri-apps/plugin-opener'
+import Logo from '@/assets/logo.png'
 import { Button } from '@/components/ui/button'
 import { env } from '@/env'
 
@@ -27,10 +28,31 @@ function RouteComponent() {
   }
 
   return (
-    <div className="flex-1 flex flex-col items-center justify-center gap-2">
-      <Button onClick={handleOpenGithubSignIn}>Sign in with GitHub</Button>
+    <div className="flex-1 flex flex-col px-10 pb-10">
+      <header>
+        <img src={Logo} alt="" className="size-9" />
+      </header>
 
-      <Button onClick={handleSignIn}>Sign in</Button>
+      <div className="flex-1 w-full max-w-sm flex flex-col mx-auto justify-center gap-10">
+        <p className="text-2xl font-bold text-center">Get started for free</p>
+
+        <div className="flex flex-col gap-4">
+          <Button onClick={handleOpenGithubSignIn} variant="outline">
+            Sign in with GitHub
+          </Button>
+
+          <Button onClick={handleSignIn} variant="outline">
+            Sign in with Google
+          </Button>
+        </div>
+      </div>
+
+      <footer className="w-full max-w-sm mx-auto">
+        {/* <p className="text-center text-xs">
+          Signing up for a Mocha account means you agree to the Privacy Policy
+          and Terms of Service.
+        </p> */}
+      </footer>
     </div>
   )
 }
